37a5b3b966f6d3d3dbbae45190708832d7801f0a
Luchko-WS <andriyluchko@gmail.com>
2020-06-21 00:43:00 +0300

    init OSM

---
+++ RiversECO-SPA/src/app/map/map.component.ts
@@ -0,0 +1,43 @@
+import Map from 'ol/Map';
+import View from 'ol/View';
+import VectorLayer from 'ol/layer/Vector';
+import Style from 'ol/style/Style';
+import Icon from 'ol/style/Icon';
+import OSM from 'ol/source/OSM';
+import * as olProj from 'ol/proj';
+import {defaults as defaultInteractions, DragRotateAndZoom} from 'ol/interaction';
+import {defaults as defaultControls, ScaleLine, FullScreen} from 'ol/control.js';
+import TileLayer from 'ol/layer/Tile';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+
+export class MapComponent implements OnInit{
+  private map: any;
+
+  ngOnInit() {
+    this.map = new Map({
+      target: 'map-layer',
+      /*interactions: defaultInteractions().extend([
+        new DragRotateAndZoom()
+      ]),*/
+      controls: defaultControls({attribution: false, zoom: true}).extend([
+        new ScaleLine(),
+        new FullScreen()
+      ]),
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: olProj.fromLonLat([28.5785, 49.2]),
+        zoom: 12
+      })
+    });
+  }
+}
