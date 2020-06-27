ae54348d1b01e95506bce9e68e35666e8350c4de
Luchko-WS <andriyluchko@gmail.com>
2020-06-27 23:04:10 +0300

    fix OSM

--- RiversECO-SPA/src/app/components/map/map.component.ts
+++ RiversECO-SPA/src/app/components/map/map.component.ts
@@ -8,9 +8,11 @@ import Style from 'ol/style/Style';
 import Icon from 'ol/style/Icon';
 import OSM from 'ol/source/OSM';
 import * as olProj from 'ol/proj';
-import {defaults as defaultInteractions, DragRotateAndZoom} from 'ol/interaction';
-import {defaults as defaultControls, ScaleLine, FullScreen} from 'ol/control.js';
+import {defaults as defaultControls, Rotate, ScaleLine} from 'ol/control';
 import TileLayer from 'ol/layer/Tile';
+
+/*
+export { default as ZoomToExtent } from './control/ZoomToExtent.js';*/
 
 import { WaterObject } from 'src/app/models/water-object';
 
@@ -34,12 +36,9 @@ export class MapComponent implements OnInit {
 
     this.map = new Map({
       target: 'map-layer',
-      /*interactions: defaultInteractions().extend([
-        new DragRotateAndZoom()
-      ]),*/
       controls: defaultControls({attribution: false, zoom: true}).extend([
-        new ScaleLine(),
-        new FullScreen()
+        new Rotate(),
+        new ScaleLine()
       ]),
       layers: [
         new TileLayer({
