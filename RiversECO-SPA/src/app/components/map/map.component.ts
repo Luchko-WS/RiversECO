2b2f38e45ba449be5d361b2a13fa6a8d78712269
Luchko-WS <andriyluchko@gmail.com>
2020-06-27 23:43:46 +0300

    add markers

--- RiversECO-SPA/src/app/components/map/map.component.ts
+++ RiversECO-SPA/src/app/components/map/map.component.ts
@@ -7,12 +7,12 @@ import VectorLayer from 'ol/layer/Vector';
 import Style from 'ol/style/Style';
 import Icon from 'ol/style/Icon';
 import OSM from 'ol/source/OSM';
+import {Point} from 'ol/geom';
+import {defaults as defaultSource, Vector} from 'ol/source';
+import Feature from 'ol/feature';
 import * as olProj from 'ol/proj';
 import {defaults as defaultControls, Rotate, ScaleLine} from 'ol/control';
 import TileLayer from 'ol/layer/Tile';
-
-/*
-export { default as ZoomToExtent } from './control/ZoomToExtent.js';*/
 
 import { WaterObject } from 'src/app/models/water-object';
 
@@ -29,11 +29,14 @@ export class MapComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.initMap();
     this.route.data.subscribe(data => {
       this.waterObjects = data['waterObjects'];
-      console.log(this.waterObjects);
+      this.addMarkers(this.waterObjects);
     });
+  }
 
+  initMap() {
     this.map = new Map({
       target: 'map-layer',
       controls: defaultControls({attribution: false, zoom: true}).extend([
@@ -50,5 +53,39 @@ export class MapComponent implements OnInit {
         zoom: 12
       })
     });
+  }
+
+  addMarkers(objects: WaterObject[]) {
+    let iconFeatures = [];
+    objects.forEach(obj => {
+      iconFeatures.push(new Feature({
+        geometry: new Point(olProj.transform([obj.lat, obj.long], 'EPSG:4326',
+        'EPSG:3857')),
+        name: obj.name,
+        population: 4000,
+        rainfall: 500
+      }));
+    });
+
+    const vectorSource = new Vector({
+      features: iconFeatures
+    });
+
+    const iconStyle = new Style({
+      image: new Icon({
+        anchor: [0.5, 46],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'pixels',
+        opacity: 1,
+        src: '../../../assets/red-marker-32.png'
+      })
+    });
+
+    const vectorLayer = new VectorLayer({
+      source: vectorSource,
+      style: iconStyle
+    });
+
+    this.map.addLayer(vectorLayer);
   }
 }
