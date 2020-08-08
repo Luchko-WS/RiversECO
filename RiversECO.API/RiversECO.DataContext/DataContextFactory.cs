a87d71524504e22ec51381c18642f6452f0619e3
Andrii Luchko <andriyluchko@gmail.com>
2020-08-08 16:32:16 +0300

    initial migration

---
+++ RiversECO.API/RiversECO.DataContext/DataContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace RiversECO.DataContext
+{
+    public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+    {
+        DataContext IDesignTimeDbContextFactory<DataContext>.CreateDbContext(string[] args)
+        {
+            var path = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).FullName, "RiversECO.API");
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(path)
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile("appsettings.Development.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<DataContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseSqlServer(connectionString);
+            return new DataContext(builder.Options);
+        }
+    }
+}
