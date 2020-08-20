8ae6f97c0d7f91477da44769a14a8859c18adbd3
Andrii Luchko <andriyluchko@gmail.com>
2020-08-20 13:27:39 +0300

    add review migration

---
+++ RiversECO.API/RiversECO.DataContext/Migrations/20200820102601_Reviews.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RiversECO.DataContext.Migrations
+{
+    public partial class Reviews : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    ModifiedOn = table.Column<DateTime>(nullable: true),
+                    Comment = table.Column<string>(nullable: false),
+                    CreatedBy = table.Column<string>(nullable: false),
+                    ModifiedBy = table.Column<string>(nullable: true),
+                    Criterias = table.Column<string>(nullable: true),
+                    WaterObjectId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reviews_WaterObjects_WaterObjectId",
+                        column: x => x.WaterObjectId,
+                        principalTable: "WaterObjects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_WaterObjectId",
+                table: "Reviews",
+                column: "WaterObjectId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reviews");
+        }
+    }
+}
